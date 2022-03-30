6276a98e54f22c32e006cf05e90608518e8b251d
petekgithub <petekshamitbeyli@gmail.com>
2022-03-30 16:51:25 +0300

    grasshopper-summation

--- kata/8kyu/are-you-playing-banjo/script.js
+++ kata/8kyu/are-you-playing-banjo/script.js
@@ -13,3 +13,6 @@ Names given are always valid strings.
 function areYouPlayingBanjo(name) {
   return name + (name[0].toLowerCase() === 'r' ? ' plays' : ' does not play') + ' banjo'
 }
+
+
+
---
+++ kata/8kyu/grasshopper-summation/script.js
@@ -0,0 +1,24 @@
+/**
+ * 
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+summation(2) -> 3
+1 + 2
+ */
+
+
+// naive solution
+var summation = function (num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+
+// better solution
+var summation = function (num) {
+  return (num * (num + 1)) / 2;
+}
