be7061af662f452c1d70b270ce5c585d355b6c9b
petekgithub <petekshamitbeyli@gmail.com>
2022-04-19 21:54:49 +0300

    list-filtering

---
+++ kata/7kyu/list-filtering/script.js
@@ -0,0 +1,14 @@
+/*
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+*/
+
+function filter_list(l) {
+  return l.filter(Number.isInteger)
+}
+
+// .filter returns a new method
+// Number.isInteger returns a integer
