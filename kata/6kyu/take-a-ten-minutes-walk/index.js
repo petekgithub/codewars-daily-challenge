d49a7d9b40a02e4f016d6de7834d263dd318ed05
petekgithub <petekshamitbeyli@gmail.com>
2022-05-23 23:52:41 +0300

    take-a-ten-minutes-walk

---
+++ kata/6kyu/take-a-ten-minutes-walk/index.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+ You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+ Note: you will always receive a valid array (string in COBOL) containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+ */ 
+
+ function isValidWalk(walk) {
+  let northSouth = 0, westEast = 0; 
+    for (let myWalk of walk) { 
+      if (myWalk == 'n') northSouth += 1; 
+      if (myWalk == 's') northSouth -= 1; 
+      if (myWalk == 'w') westEast += 1; 
+      if (myWalk == 'e') westEast -= 1; 
+    } 
+
+    return walk.length == 10 && northSouth === 0 && westEast === 0; 
+}
