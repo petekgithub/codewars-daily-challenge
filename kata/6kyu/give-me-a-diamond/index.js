bf9839a4ac9e1c1987cddfd4bae886a410529d69
petekgithub <petekshamitbeyli@gmail.com>
2022-07-07 23:51:21 +0300

    give-me-a-diamond

--- kata/6kyu/give-me-a-diamond/index.js
+++ kata/6kyu/give-me-a-diamond/index.js
@@ -1,12 +1,12 @@
 function diamond(n){
   if ( n < 1 || n % 2 === 0 ) return null
-  let result = '', middle = Math.ceil(n/2);
+  let final = '', middle = Math.ceil(n/2);
   for ( let i = 1; i <= n; i++ ) {
     if ( i <= middle ) {
-      result += ' '.repeat(middle-i) + '*'.repeat(2*i-1) + '\n'
+      final += ' '.repeat(middle-i) + '*'.repeat(2*i-1) + '\n'
     } else {
-      result += ' '.repeat(i-middle) + '*'.repeat(2*(n-i)+1) + '\n'
+      final += ' '.repeat(i-middle) + '*'.repeat(2*(n-i)+1) + '\n'
     }
   }
-  return result;
+  return final;
 }
