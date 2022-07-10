ae0d3b81c387cc9382a370746ba2c58f8490514d
petekgithub <petekshamitbeyli@gmail.com>
2022-07-10 23:37:38 +0300

    extract-the-domain-name-from-a-url

--- kata/5kyu/extract-the-domain-name-from-a-URL/index.js
+++ kata/5kyu/extract-the-domain-name-from-a-URL/index.js
@@ -1,3 +1,6 @@
 function domainName(url){
-  return url.replace('http://', '').replace('https://', '').replace('www.', '').split('.')[0];
+  return url.replace('http://', '')
+  .replace('https://', '')
+  .replace('www.', '')
+  .split('.')[0];
 }
