07224c2fbdcc0ae4582805e3f90926b3522d9ab6
petekgithub <petekshamitbeyli@gmail.com>
2022-08-17 01:02:33 +0300

    update-v3

--- kata/7kyu/reversed-words/index.js
+++ kata/7kyu/reversed-words/index.js
@@ -2,11 +2,3 @@ function reverseWords(str) {
   return str.split('').reverse().join('').split(' ').reverse().join(' ');
 }
 
-// delete it later
-function reverseWords(str) {
-  return str.split('').reverse().join('').split(' ').reverse().join(' ');
-}
-// delete it later
-function reverseWords(str) {
-  return str.split('').reverse().join('').split(' ').reverse().join(' ');
-}
